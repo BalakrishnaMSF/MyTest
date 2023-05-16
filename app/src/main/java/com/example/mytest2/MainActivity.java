6d214c7fe77934b87dc124d91aa43180a2d18ad1
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-05-16 14:44:21 +0530

    I used the RecyclerView

--- app/src/main/java/com/example/mytest2/MainActivity.java
+++ app/src/main/java/com/example/mytest2/MainActivity.java
@@ -1,27 +1,48 @@
 package com.example.mytest2;
 
+
 import android.os.Bundle;
-import android.app.Activity;
-import android.os.PersistableBundle;
-import android.view.Menu;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends Activity {
-    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstancestate) {
+        super.onCreate(savedInstancestate);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                R.layout.activity_listview, mobileArray);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
 
-        ListView listView = (ListView) findViewById(R.id.mobile_list);
-        listView.setAdapter(adapter);
+        List<Item> items = new ArrayList<>();
+        items.add(new Item("Bala","bala@gmail",R.drawable.ic_launcher_foreground));
+        items.add(new Item("Krishna","krishna@gmail",R.drawable.ic_launcher_foreground));
+        items.add(new Item("Mahesh","mahesh@gmail",R.drawable.ic_launcher_foreground));
+        items.add(new Item("Meghana","meghana@gmail",R.drawable.ic_launcher_foreground));
+        items.add(new Item("Manasa","Manasa@gmail",R.drawable.ic_launcher_foreground));
+        items.add(new Item("Jasmin","jasmin@gmail",R.drawable.ic_launcher_foreground));
+        items.add(new Item("Divya","divya@gmail",R.drawable.ic_launcher_foreground));
+        items.add(new Item("Sumaiya","sumaiya@gmail",R.drawable.ic_launcher_foreground));
+        items.add(new Item("Bala_123","bala@gmail",R.drawable.ic_launcher_background));
+        items.add(new Item("Krishna_143","krishna@gmail",R.drawable.ic_launcher_background));
+        items.add(new Item("Mahesh_132","mahesh@gmail",R.drawable.ic_launcher_background));
+        items.add(new Item("Meghana_123","meghana@gmail",R.drawable.ic_launcher_background));
+        items.add(new Item("Manasa_032","Manasa@gmail",R.drawable.ic_launcher_background));
+        items.add(new Item("Jasmin_143","jasmin@gmail",R.drawable.ic_launcher_background));
+        items.add(new Item("Divya_132","divya@gmail",R.drawable.ic_launcher_background));
+        items.add(new Item("Sumaiya_53","sumaiya@gmail",R.drawable.ic_launcher_background));
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(getApplicationContext(),items));
+
     }
+
+
 }
