e864765a68aca2a046c6f304802e8ea703e9b92d
balakrishna.g <balakrishna.s@marketsimplified.com>
2023-05-19 17:13:00 +0530

    Simple calculator program

--- app/src/main/java/com/example/mytest2/MainActivity.java
+++ app/src/main/java/com/example/mytest2/MainActivity.java
@@ -1,129 +1,91 @@
 package com.example.mytest2;
 
-import android.content.DialogInterface;
-import android.database.Cursor;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editUserId;
-    EditText editUserName;
-    EditText editUserPassword;
-    DatabaseManager dbManager;
-
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        editUserId = (EditText) findViewById(R.id.editTextID);
-//        editUserName = (EditText) findViewById(R.id.editTextUserName);
-//        editUserPassword = (EditText) findViewById(R.id.editTextPassword);
-//
-//        dbManager =  new DatabaseManager(this);
-//        try {
-//            dbManager.open();
-//        }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
-//    }
-
-    public void btnInsertPressed(View v){
-        dbManager.insert(Integer.parseInt(editUserId.getText().toString()),editUserName.getText().toString(), editUserPassword.getText().toString());
-        Toast.makeText(this, "Data inserted", Toast.LENGTH_SHORT).show();
-
-
-    }
-
-    public void btnFetchPressed(View v){
-
-        Cursor cursor = dbManager.fetch();
-
-        if(cursor.moveToFirst()){
-            do{
-                String ID = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.USER_ID));
-                String username = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.USER_NAME));
-                String password = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.USER_PASSWORD));
-                Log.i("DATABASE_TAG","I have read ID : " + ID + " Username "+ username+ "password :" + password);
-                Toast.makeText(this, "Data fetched", Toast.LENGTH_SHORT).show();
-
-            }while (cursor.moveToNext());
-        }
-
-    }
-
-    public void btnUpdatePressed(View v){
-
-        dbManager.update(Long.parseLong(editUserId.getText().toString()), editUserName.getText().toString(), editUserPassword.getText().toString());
-        Toast.makeText(this, "Data Updated", Toast.LENGTH_SHORT).show();
-
-    }
-
-    public void btnDeletePressed(View v){
-
-        dbManager.delete(Long.parseLong(editUserId.getText().toString()));
-        Toast.makeText(this, "Data deleted", Toast.LENGTH_SHORT).show();
-
-    }
-
-
-    Button closeButton;
-    AlertDialog.Builder builder;
+    Button add,sub,mul,div;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        add = (Button) findViewById(R.id.button1);
+        sub = (Button) findViewById(R.id.button2);
+        mul = (Button) findViewById(R.id.button3);
+        div = (Button) findViewById(R.id.button4);
 
-        setContentView(R.layout.activity_main);
-        editUserId = (EditText) findViewById(R.id.editTextID);
-        editUserName = (EditText) findViewById(R.id.editTextUserName);
-        editUserPassword = (EditText) findViewById(R.id.editTextPassword);
-
-        dbManager =  new DatabaseManager(this);
-        try {
-            dbManager.open();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-        closeButton = (Button) findViewById(R.id.button);
-        builder = new AlertDialog.Builder(this);
-        closeButton.setOnClickListener(new View.OnClickListener() {
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText et1 = (EditText) findViewById(R.id.number1);
+                EditText et2 = (EditText) findViewById(R.id.number2);
+                EditText et3 = (EditText) findViewById(R.id.number3);
+                int n1 = Integer.parseInt(et1.getText().toString());
+                int n2 = Integer.parseInt(et2.getText().toString());
+                int result = n1 + n2;
 
-                builder.setMessage(R.string.dialog_message) .setTitle(R.string.dialog_title);
-                builder.setMessage("Data inserted , Do you want to close click Exit or to add another data click OK")
-                        .setCancelable(false)
-                        .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                finish();
-                                Toast.makeText(getApplicationContext(),"you choose to exit from Application",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                        .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                                Toast.makeText(getApplicationContext(),"you choose to add another data/operations",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                AlertDialog alert = builder.create();
-                alert.setTitle("AlertDialogExample");
-                alert.show();
+                et3.setText("Total value "+ result);
+
             }
         });
 
+
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText et1 = (EditText) findViewById(R.id.number1);
+                EditText et2 = (EditText) findViewById(R.id.number2);
+                EditText et3 = (EditText) findViewById(R.id.number3);
+
+                int n1 = Integer.parseInt(et1.getText().toString());
+                int n2 = Integer.parseInt(et2.getText().toString());
+                int result = n1 - n2;
+
+                et3.setText("Total value "+ result);
+
+
+            }
+        });
+
+
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText et1 = (EditText) findViewById(R.id.number1);
+                EditText et2 = (EditText) findViewById(R.id.number2);
+                EditText et3 = (EditText) findViewById(R.id.number3);
+
+                int n1 = Integer.parseInt(et1.getText().toString());
+                int n2 = Integer.parseInt(et2.getText().toString());
+                int result = n1 * n2;
+
+                et3.setText("Total value "+ result);
+            }
+
+        });
+
+
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText et1 = (EditText) findViewById(R.id.number1);
+                EditText et2 = (EditText) findViewById(R.id.number2);
+                EditText et3 = (EditText) findViewById(R.id.number3);
+
+                int n1 = Integer.parseInt(et1.getText().toString());
+                int n2 = Integer.parseInt(et2.getText().toString());
+                int result = n1 / n2;
+
+                et3.setText("Total value "+ result);
+
+            }
+        });
     }
 }
